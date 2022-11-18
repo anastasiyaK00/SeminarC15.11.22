6e7335af075e850158b4d86a1418ff5037b3216d
Anastasiya <anastasiyak96.96@mail.ru>
2022-11-18 22:00:52 +0300

    01

--- 01/Program.cs
+++ 01/Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Обмен значениями двух переменных.
+/* 1)Классичский способ
+int a = 5;
+int b = 7;
+int temp;
+temp=a;
+a=b;
+b=temp;
+*/
+
+// 2)Без использования третей переменной
+/*
+int a =5;
+int b=7;
+a=a+b;
+b=a-b;
+a=a-b;
+*/
+
+// 3) Используя ксор ^
+int a = 5;
+int b = 7;
+a=a^b;
+b=a^b;
+a=a^b;
+System.Console.WriteLine($"a={a}, b={b}");
